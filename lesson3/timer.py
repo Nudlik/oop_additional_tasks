c7acb59ce94082a949e736c6be9e8bc228032d14
Nudlik <gegorp@mail.com>
2023-08-29 19:37:09 +0700

    lesson 3 timer.py

--- lesson3/timer.py
+++ lesson3/timer.py
@@ -5,13 +5,22 @@
 - __exit__(self, exc_type, exc_val, exc_tb): магический метод, который останавливает таймер
 и выводит время выполнения блока кода.
 """
+import time
 
 
 class Timer:
-    pass
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.end_time = time.time()
+            self.elapsed_time = self.end_time - self.start_time
 
 
 with Timer() as timer:
-    # блок кода
+    res = sum(i ** 99 for i in range(1000000))
 
 print("Execution time:", timer.elapsed_time)
